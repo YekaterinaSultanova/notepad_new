de51eabafc0a9d987830d0af7c828663bdf1e3a6
YekaterinaSultanova <yekaterina.sultanova@ctco.lv>
2018-11-20 01:45:06 +0200

    Notepad: basic functions implementation

--- src/com/notepad/Main.java
+++ src/com/notepad/Main.java
@@ -1,32 +1,61 @@
 package com.notepad;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     static Scanner scanner = new Scanner(System.in);
-    static List<>
+    static List<Person> records = new ArrayList<>();
 
     public static void main(String[] args) {
         for (; ; ) {
-            System.out.println("cmd: ");
+            System.out.println("What do you want to do?");
             String cmd = scanner.next();
             switch (cmd) {
-                case "create"
-                    ;
+                case "create":
                     createPerson();
                     break;
-                case "help"
-                    ;
+                case "help":
                     showHelp();
                     break;
-                case "List":
-                    list();
+                case "list":
+                    showList();
                     break;
-                case "exit";                    ;
+                case "exit":
                     return;
-                default;
-                    System.out.println("Wrong command. Try help");
+                default:
+                    System.out.println("Wrong command. Try 'help'");
             }
         }
+    }
+
+    private static void showList() {
+        records.forEach(person -> {
+            String name = person.getName();
+            String surname = person.getSurname();
+            String phoneNumber = person.getPhoneNumber();
+
+            System.out.println(name + " " + surname + " " + phoneNumber);
+        });
+    }
+
+    private static void showHelp() {
+        System.out.println("create - create new person");
+        System.out.println("list - show list of persons");
+        System.out.println("exit - exit from program");
+
+    }
+
+    private static void createPerson() {
+        System.out.println("Name: ");
+        String name = scanner.next();
+        System.out.println("Surname: ");
+        String surname = scanner.next();
+        System.out.println("Phone number: ");
+        String phoneNumber = scanner.next();
+
+        Person person = new Person(name, surname, phoneNumber);
+        records.add(person);
     }
 }
--- src/com/notepad/Person.java
+++ src/com/notepad/Person.java
@@ -1,4 +1,28 @@
 package com.notepad;
 
 public class Person {
+
+    private String name;
+
+    private String surname;
+
+    private String phoneNumber;
+
+    public Person(String name, String surname, String phoneNumber) {
+        this.name = name;
+        this.surname = surname;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
 }
