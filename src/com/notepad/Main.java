b9bc37c256c791fdda4d3deebf80b5b3a5f3f808
y.sultanova01 <yekaterina.sultanova@ctco.lv>
2018-11-20 17:04:17 +0200

    added new function: delete added new attribute: email

--- src/com/notepad/Main.java
+++ src/com/notepad/Main.java
@@ -1,11 +1,16 @@
 package com.notepad;
 
+import com.sun.org.apache.xpath.internal.SourceTree;
+import com.sun.xml.internal.bind.v2.model.core.ID;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
     static Scanner scanner = new Scanner(System.in);
+    public static final String EMAIL = scanner.next();
     static List<Person> records = new ArrayList<>();
 
     public static void main(String[] args) {
@@ -22,6 +27,9 @@ public class Main {
                 case "list":
                     showList();
                     break;
+                case "delete":
+                    deleteId();
+                    return;
                 case "exit":
                     return;
                 default:
@@ -32,11 +40,7 @@ public class Main {
 
     private static void showList() {
         records.forEach(person -> {
-            String name = person.getName();
-            String surname = person.getSurname();
-            String phoneNumber = person.getPhoneNumber();
-
-            System.out.println(name + " " + surname + " " + phoneNumber);
+            System.out.println(person);
         });
     }
 
@@ -44,18 +48,43 @@ public class Main {
         System.out.println("create - create new person");
         System.out.println("list - show list of persons");
         System.out.println("exit - exit from program");
+        System.out.println("delete - ID deletion");
 
     }
 
     private static void createPerson() {
         System.out.println("Name: ");
         String name = scanner.next();
+
         System.out.println("Surname: ");
         String surname = scanner.next();
+
         System.out.println("Phone number: ");
         String phoneNumber = scanner.next();
 
+        if (phoneNumber) = Random rand;
+        rand.nextInt(5) + 1;
+        long t1 = System.currentTimeMillis();
+        for (int i = 1; i <= 5; i++) {
+            int phoneNumber = readPhoneNumber();
+            if (phoneNumber == PhoneNumber) {}
+
+        }
+        while ("")
+
+
+
+        System.out.println("Email: ");
+        String email ????? = scanner.next();
+
         Person person = new Person(name, surname, phoneNumber);
         records.add(person);
+    }
+
+    private static void deleteId() {
+        System.out.println("Which ID to delete?");
+        ???????
+        System.out.println("ID " + "is deleted");
+
     }
 }
--- src/com/notepad/Person.java
+++ src/com/notepad/Person.java
@@ -1,28 +1,48 @@
 package com.notepad;
 
 public class Person {
+    private static int counter = 0;
+    private int id; //ID mozhno toljko 4itatj
 
     private String name;
-
     private String surname;
-
     private String phoneNumber;
+    private String email;
+
+    public Person() {
+        counter++;
+        id = counter;
+    }
 
     public Person(String name, String surname, String phoneNumber) {
+        this();
         this.name = name;
         this.surname = surname;
         this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
     }
 
     public String getName() {
         return name;
     }
-
     public String getSurname() {
         return surname;
     }
-
     public String getPhoneNumber() {
         return phoneNumber;
+    }
+    public String getEmail() {return email; }
+
+
+    public Person(int id, String name, String surname, String phoneNumber, String email) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
     }
 }
